89188099a5f1f6726eac2c4f7b4de543d49a9881
Hendoe <hendo1349@gmail.com>
2020-08-13 14:28:30 -0500

    new functions

--- scripts/item.js
+++ scripts/item.js
@@ -1,4 +1,25 @@
 'use strict';
 
+function validateName(name) {
+    try {
+        if(!name) {
+            throw new TypeError('Name must not be blank')
+        } 
+     } catch(error) {
+            throw new TypeError('Name must not be blank')
+     }
+}
 
-export default {};
+function create(name) {
+    return {
+        id: cuid(),
+        name: name,
+        checked: false,
+    };
+}
+
+
+export default {
+    validateName,
+    create,
+};
